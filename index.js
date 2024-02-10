01bc77aab02b00737dac21c054a2ca1734b1ca1c
linjie <jie355116@gmail.com>
2024-02-10 23:49:33 +0800

    chromeOptions
    chromeOptions, logging

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const {Builder, Browser, By, Key, until, logging} = require('selenium-webdriver');
+const { Options } = require("selenium-webdriver/chrome");
 
 (async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  let driver = await new Builder().forBrowser(Browser.CHROME)
+                    .setChromeOptions(new Options().setChromeBinaryPath('/Applications/Google Chrome69.app/Contents/MacOS/Google Chrome'))
+                    .build();
+
+  // logging.installConsoleHandler()
+  // logging.getLogger(`${logging.Type.DRIVER}.http`).setLevel(logging.Level.ALL)
   try {
     await driver.get('https://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
